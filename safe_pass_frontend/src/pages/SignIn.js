9522f4be5ceb788c94b085d28d274613a72efdf5
Sithum-U <avsithum@gmail.com>
2023-10-03 16:51:21 +0530

    Prevented Insecure Design

--- safe_pass_frontend/src/pages/SignIn.js
+++ safe_pass_frontend/src/pages/SignIn.js
@@ -20,8 +20,8 @@ import Footer from "../component/Footer";
 const SignIn = ({ history }) => {
   const { t } = useTranslation();
   const [values, setValues] = useState({
-    email: "dulnathamerasinghe@gmail.com",
-    password: "Dulnath@123",
+    email: "",
+    password: "",
   });
   const [visible, setVisible] = useState(false);
   const [passwordStartedTyping, setPasswordStartedTyping] = useState(false); // Track password input
